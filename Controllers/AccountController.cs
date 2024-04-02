8fe1c7a4f85e2fe1feccf777371f2390ad185516
Bahaa Magdy <156998666+bahaamagdygit@users.noreply.github.com>
2024-04-02 05:17:49 +0200

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,126 @@
+﻿using Library.Context;
+using Library.Dtos.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Mvc.Controllers
+{
+    
+    public class AccountController : Controller
+    {
+        // هنا بعمل انجكت لل يوزر منجر والساين ان مانجر
+        private readonly UserManager<NewUser> userManager;
+        private readonly SignInManager<NewUser> signInManager;
+
+      
+        public AccountController(UserManager<NewUser> _userManager, SignInManager<NewUser> _signInManager, RoleManager<IdentityRole> _roleManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+          
+
+
+
+        }
+
+
+
+		[HttpGet]
+		public IActionResult Registration()
+		{
+
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Registration(RegistrationDTO RegModel)
+		{
+
+			if (ModelState.IsValid)
+			{
+				NewUser user = new NewUser();
+				user.Email = RegModel.Email;
+				user.UserName = RegModel.Name;
+				user.PhoneNumber = RegModel.PhoneNumber;
+				user.UserName = RegModel.UserName;
+				user.IsDeleted = false;
+				//هنا بيعمل هاش للباسورد قبل مبيسيفه فالداتا بيز 
+				var result = await userManager.CreateAsync(user, RegModel.Password);
+				if (result.Succeeded)
+				{
+					//create cookie 
+					// the false here for persistant
+					// ياعني لو كانتر بترو هيبقا ليها كوكي مدتها كام يوم لاكن لو كانت فولس هتبقا الكوكي عل السشن
+
+					var res = await userManager.AddToRoleAsync(user, RegModel.RoleName.ToString().ToLower());
+					await signInManager.SignInAsync(user, false);
+					return RedirectToAction("Login", "Account");
+				}
+				else
+				{
+					// هنا بنلف علي الايرور  
+					foreach (var errItem in result.Errors)
+					{
+						ModelState.AddModelError("err", errItem.Description);
+					}
+
+					return View(RegModel);
+				}
+
+
+
+			}
+			return View(RegModel);
+		}
+
+
+
+
+		[HttpGet]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+
+          //ممكن هنا نوجه اليوزر للصفحه الي كان اوذ يروح ليها 
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO login, string returnUrl = null)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+            NewUser user = await userManager.FindByNameAsync(login.UserName);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Username/Email or Password incorrect");
+                return View();
+            }
+            var result = signInManager.CheckPasswordSignInAsync(user, login.Password, true).Result;
+            if (result.IsLockedOut)
+            {
+                ModelState.AddModelError(string.Empty, "Try again later"); return View(result);
+            }
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Username/Email or Password incorrect");
+            }
+            await signInManager.SignInAsync(user, login.remember);
+            var userROle = await userManager.GetRolesAsync(user);
+            return RedirectToAction("index", "book");
+
+        }
+       
+
+        public async Task<IActionResult> logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+    }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,219 @@
+﻿using Library.Application.Services;
+using Library.Dtos.Book;
+using Library.Models;
+using Library.Mvc.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace Library.Mvc.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
+        public BookController(IBookService bookService , IAuthorService authorService)
+        {
+            _bookService = bookService;
+            _authorService = authorService;
+        }
+        // GET: BookController
+        public async Task<ActionResult> Index()
+        {
+            var Books = await _bookService.GetAllPagination(10, 1);
+            return View(Books);
+        }
+
+       
+
+        // GET: BookController/Create
+        [Authorize]
+        public async Task<ActionResult> Create()
+        {
+            var Books = await _authorService.GetAllAuthor();
+            ViewBag.Authors = Books.Entities.ToList();
+
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CreateOrUpdateBookDTO Book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var Res = await _bookService.Create(Book);
+                    if (Res.IsSuccess)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ViewBag.Error = Res.Message;
+                        return View(Book);
+                    }
+                }
+                else
+                {
+                    return View(Book);
+
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        [Authorize]
+        public async Task<ActionResult> Edit(int id)
+        {
+            try
+            {
+                
+                var book = await _bookService.GetOne(id);
+
+              
+                if (book != null)
+                {
+                    var authors = await _authorService.GetAllAuthor();
+                    ViewBag.Authors = authors.Entities.ToList();
+
+                   
+                    return View(book);
+                }
+                else
+                {
+                  
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "An error occurred while processing your request.";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        
+        public async Task<ActionResult> Edit(int id, CreateOrUpdateBookDTO Book)
+        {
+
+             var authors = await _authorService.GetAllAuthor();
+            ViewBag.Authors = authors.Entities.ToList();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _bookService.Update(Book);
+
+                    if (result.IsSuccess)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ViewBag.Error = result.Message;
+                        return View(Book);
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Invalid model state.";
+                    return View(Book);
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                ViewBag.Error = "An error occurred while processing your request.";
+                return View(Book);
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public async Task<ActionResult> HardDelete(int id)
+        {
+            var prd = await _bookService.GetOne(id);
+            return View(prd);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> HardDelete(int id, CreateOrUpdateBookDTO Book)
+        {
+            try
+            {
+                var p = await _bookService.HardDelete(Book);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ViewBag.Error = "An unexpected error occurred.";
+                return View();
+
+
+            }
+        }
+
+
+
+
+
+
+        public async Task<IActionResult> AddtoCart(int Id)
+        {
+
+           
+            if (Id == 0)
+            {
+                return BadRequest("Invalid list of IDs");
+            }
+
+            // هات الكوكي او اعمل وحده جديده
+            var cookie = Request.Cookies["BookListIDs"];
+            List<int> existingIds = new List<int>();
+            if (!string.IsNullOrEmpty(cookie))
+            {
+                // DeserializeObject بتلغي تسلسل الايديهات
+                existingIds = JsonConvert.DeserializeObject<List<int>>(cookie);
+            }
+            existingIds.Add(Id);
+
+            List<int> uniqueIds = existingIds.Distinct().ToList(); // بتمسح المتكرر
+
+            // بتعيد تسلسل الايديهات مره تاني في الجيسون
+            string serializedIds = JsonConvert.SerializeObject(uniqueIds);
+            // هنحط الليست دي فالكوكيز
+            Response.Cookies.Append("BookListIDs", serializedIds);
+
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> SHowCartprouct()
+        {
+            // بستدعي ليستت الجيسون الي فالكوكي
+            var cookie = Request.Cookies["BookListIDs"];
+            List<int> existingIds = new List<int>();
+            if (!string.IsNullOrEmpty(cookie))
+            {
+               
+                existingIds = JsonConvert.DeserializeObject<List<int>>(cookie);
+            }
+            var model = await _bookService.GetProductListByIdList(existingIds);
+
+            return View(model);
+        }
+
+    }
+}
